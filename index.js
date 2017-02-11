c915601ecfe180b007c4e5ee20ab840dc3593a52
Sanjay Bhatikar <sanjay.bhatikar@gmail.com>
2017-02-11 02:26:51 +0000

    Cleaned up sever

--- index.js
+++ index.js
@@ -12,26 +12,6 @@ app.use('/api/v1/', require("./api.js")(wagner));
 app.listen(process.env.PORT);
 console.log("Listening on port: " + process.env.PORT);
 
-// var wagner = require("wagner-core")
-// var Alert;
-// var myAlert;
-
-// var models = require('./models.js')(wagner);
-// Alert = models.Alert;
-
-// var myAlert = new Alert({
-//     _id         : 997,
-//     Note        : "I went to meet Roger Pena at Cafe Santa Maria on 9th Main",
-//     Email       : ['sanjay.bhatikar@gmail.com', 'zarthustra7@gmail.com'],
-//     Trigger     : new Date(2016, 12, 01),
-//     Active      : true,
-// })
-
-// myAlert.save(function(err, doc) {
-//     if (err) throw err;
-//     console.log("Saved: " + doc);
-// });
-
 // var mails = require("./mails.js")(wagner);
 // var mailgun = mails.mailgun;
 
